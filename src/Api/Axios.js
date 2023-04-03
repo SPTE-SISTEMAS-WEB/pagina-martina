ad2122f4dc20c6fa1646e84efa7ddcb1fbbd85e0
soporte sistema <117922224+SPTE-SISTEMAS-WEB@users.noreply.github.com>
2023-04-03 16:42:15 -0500

    CAMBIO DE CONFIGURACION

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-// const baseURL = import.meta.env.VITE_APP_URL
+const baseURL = import.meta.env.VITE_APP_URL
 
 export const instanciaAxios = axios.create({
     // local
-    baseURL: `http://192.168.10.73:82/api`,
+    baseURL: `${baseURL}/api`,
     // produccion
     // baseURL: "http://181.188.196.2:9090/api",
     headers: {
