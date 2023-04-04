0abfaa83914c0558f4a359ffefeb07f2699fe580
soporte sistema <117922224+SPTE-SISTEMAS-WEB@users.noreply.github.com>
2023-04-04 10:13:09 -0500

    AGREGADO DE URL

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_APP_URL
+// const baseURL = import.meta.env.VITE_APP_URL
 
 export const instanciaAxios = axios.create({
     // local
-    baseURL: `${baseURL}/api`,
+    baseURL: `//181.188.196.11:1015/api`,
     // produccion
     // baseURL: "http://181.188.196.2:9090/api",
     headers: {
